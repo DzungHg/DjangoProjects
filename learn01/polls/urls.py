1b0075ec21124cfdd21b8eef0cd69d8792cf3903
DzungHg <hg.dzung@gmail.com>
2019-06-15 06:54:19 +0700

    t

--- learn01/polls/urls.py
+++ learn01/polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     #ẽ: /polls/
     path('', views.index, name='index'),
--- learn01/polls/views.py
+++ learn01/polls/views.py
@@ -1,15 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 from django.http import HttpResponse
+
+
+from .models import Question
 # Create your views here.
 def index(request):
-    return  HttpResponse("Chào mừng Hoàng Văn Dũng.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+#còn lại chưa thay đổi
 
 def detail(request, question_id):
-    return HttpResponse("You are looking question %s." %question_id)
+    question = get_list_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("you're votting on question %s." question_id)
+    return HttpResponse("you're votting on question %s." %question_id)
