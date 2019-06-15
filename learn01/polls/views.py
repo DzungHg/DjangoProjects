39004e067e1e9c4f8763f2a753a3d07bb4482330
DzungHg <hg.dzung@gmail.com>
2019-06-15 17:20:29 +0700

    hoc tiep
    dz

--- learn01/polls/views.py
+++ learn01/polls/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import get_list_or_404, render
-from django.http import HttpResponse
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-
-from .models import Question
+from .models import Choice, Question
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -19,8 +19,21 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question_id})
 
 def vote(request, question_id):
-    return HttpResponse("you're votting on question %s." %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question form.
+        return render(request, 'polls/detail.html', {'question': question,
+                                                    'error_message': "You didn't not select a choice",})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
